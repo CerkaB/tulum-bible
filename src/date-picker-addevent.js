30ea52b28cf4f311adfc8ff3e1baa06f9f3ec6d4
Boris <cerkab@outlook.com>
2024-06-27 18:38:37 -0500

    Updates to date pickers

--- src/date-picker-addevent.js
+++ src/date-picker-addevent.js
@@ -1,22 +1,9 @@
 import AirDatepicker from 'air-datepicker';
 import localeEn from 'air-datepicker/locale/en';
 
-// Get the input element
-const inputElement = document.querySelector('#date-picker');
-// Create a hidden input to store the Airtable-compatible date
-const hiddenInputForAirtable = document.createElement('input');
-hiddenInputForAirtable.type = 'hidden';
-hiddenInputForAirtable.name = 'airtableDate';
-document.body.appendChild(hiddenInputForAirtable);
-
-new AirDatepicker(inputElement, {
+new AirDatepicker('#date-picker', {
     locale: localeEn,
-    dateFormat: 'd/mmm/yy', // User-friendly format for display
+    dateFormat: 'dd/MMM/yy',
     firstDay: 1,
     minDate: new Date(),
-    onSelect({date}) {
-        // Format the date as YYYY-MM-DD for Airtable and store it in the hidden input
-        const formattedDateForAirtable = date.toISOString().split('T')[0];
-        hiddenInputForAirtable.value = formattedDateForAirtable;
-    }
 });
